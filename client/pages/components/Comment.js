ef2294b52553a622bd839f4f9011fdd74a9da899
0nrK <onurkirnapci@gmail.com>
2021-12-06 20:39:35 +0300

    edit comment section

--- client/pages/components/Comment.js
+++ client/pages/components/Comment.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Image from 'next/dist/client/image'
 
 const Comment = () => {
@@ -11,43 +11,40 @@ const Comment = () => {
 
     return (
         <>
-        <div className="flex flex-col h-auto p-1 mt-3 bg-gray-600">
-            <div className="flex  rounded-xl">
-                    <div className="p-1 ">
+            <div className="flex flex-col h-auto p-1 mt-3 ">
+                <div className="flex rounded-xl w-full">
+                    <div className="p-1 w-16">
                         <Image
-                        src="/friend2.jpeg"
-                        alt="userpp"
-                        width={56}
-                        height={56}
-                        className="rounded-full"
+                            src="/friend2.jpeg"
+                            alt="userpp"
+                            width={56}
+                            height={56}
+                            className="rounded-full cursor-pointer"
                         />
                     </div>
-                    <div className="flex flex-col text-xs ml-3 w-5/6">
-                        <div className="flex items-center justify-between">
-                            <span className="text-2xl font-bold pt-1 ">Mike Dorsey</span>
-                            <span className="text-xl text-gray-400 pt-1">5 minutes ago</span>
+                    <div className="bg-gray-600 rounded-lg w-5/6">
+                        <div className="flex flex-col text-xs pt-1 ml-3 w-5/6">
+                            <div className="flex flex-col justify-between">
+                                <span className="text-2xl cursor-pointer font-bold">Mike Dorsey</span>
+                            </div>
+                            <span className="text-xl text-white p-1 pb-3 break-words">AAAAAAAAAAAAAAAAAAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaaaAAAAAAAAAAAA</span>
                         </div>
-                            <span className="text-xl text-white p-1 break-words">AAAAAAAAAAAAAAAAAAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaAAAAaaaaAAAAAAAAAAAA</span>
                     </div>
-                    {/*  <div onClick={handleLike} className="h-16 w-24 relative top-3 right-0  flex items-center justify-center bg-gray-800 rounded-full ml-52 hover:scale-105">
-                        <button>
-                            <svg className={hasLiked ? "w-6 h-6 text-green-600" : "w-6 h-6"} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path>
-                            </svg>
-                        </button>
-                    </div>  */}
-            </div>
-                <div className="flex ml-16 items-center">
-                    <div className="w-20 flex justify-center bg-gray-800">
+                </div>
+                <div className="flex ml-16  justify-between items-center">
+                    <div className="ml-1 mt-3">
                         <button className={hasLiked ? "mr-3 text-2xl text-green-600 " : "mr-3 text-2xl"} onClick={handleLike}>Like</button>
+                        <span className="text-2xl mr-3">·</span>
+                        <span className="text-2xl">5m</span>
                     </div>
-                    <div className="ml-3 ">
+                    <div className="mr-16 flex">
                         <svg className={hasLiked ? "w-6 h-6 text-green-600" : "w-6 h-6"} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path>
                         </svg>
+                        <span className="text-xl ml-1">7</span>
                     </div>
                 </div>
-        </div>
+            </div>
         </>
     )
 }
--- client/pages/components/Post.js
+++ client/pages/components/Post.js
@@ -37,7 +37,7 @@ const Post = ({name, id, content}) => {
                     className="rounded-full"
                     />
                     <div className="flex flex-col ml-3">
-                        <span className="text-xl font-bold">{name}</span>
+                        <span className="text-2xl font-bold">{name}</span>
                         <span className="text-sm">3 days ago</span>
                     </div>
                 </div>
@@ -55,21 +55,21 @@ const Post = ({name, id, content}) => {
                 {/* Like button */}
                 <button 
                 onClick={handleLike}
-                className={hasLiked ? "flex items-center justify-center text-green-600 w-1/3" 
+                className={hasLiked ? "flex items-center hover:bg-gray-500 p-4 justify-center text-green-600 w-1/3" 
                 :
-                "flex items-center justify-center  w-1/3 hover:bg-gray-500"}>
+                "flex items-center justify-center p-4  w-1/3 hover:bg-gray-500"}>
                     <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path>
                     </svg>
                 </button>
                 {/* Comment button */}
-                <button onClick={handleFocus} className="flex items-center justify-center h-12 w-1/3 hover:bg-gray-500">
+                <button onClick={handleFocus} className="flex items-center p-4 justify-center w-1/3 hover:bg-gray-500">
                     <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path>
                     </svg>
                 </button>
                 {/* Share button */}
-                <button className="flex items-center justify-center h-12 w-1/3  hover:bg-gray-500">
+                <button className="flex items-center justify-center  p-4 w-1/3  hover:bg-gray-500">
                     <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"></path>
                     </svg>
